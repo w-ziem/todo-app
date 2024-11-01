93360e75c2d00132234d677160feab82fb9ed80e
w-ziem <143935791+w-ziem@users.noreply.github.com>
2024-11-01 12:39:40 +0100

    Add task completion functionality and different tasks list

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const newTaskBtn = document.querySelector(".new-task-btn");
 const taksInput = document.querySelector(".task-input");
 const tasksList = document.querySelector(".tasks");
-const tasks = document.querySelectorAll("li");
+const optionSelection = document.querySelector(".lists-select-menu");
 
 
 
@@ -10,7 +10,15 @@ const tasks = document.querySelectorAll("li");
 
 
 //add new task on button click
-newTaskBtn.addEventListener("click", () => {
+newTaskBtn.addEventListener("click", createTask);
+taksInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        createTask();
+    }
+});
+
+
+function createTask(){
     const newTask =  taksInput.value;
     if (newTask === '') {
         alert("make sure to type something in.");  
@@ -20,9 +28,66 @@ newTaskBtn.addEventListener("click", () => {
     //TODO
     //add a way to choose groups while creating task
     newListItem.innerHTML = newTask;
+    newListItem.classList.add(optionSelection.value);
+    newListItem.classList.add("task");
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-button");
+    removeButton.innerHTML = 'x';
+    newListItem.appendChild(removeButton);
     tasksList.appendChild(newListItem);
     taksInput.value = '';
+}
+
+//add event listeners to tasks that toggles thier state from finished
+tasksList.addEventListener("click", (e) => {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("finished"); 
+    } 
+    if(e.target.tagName === "BUTTON") {
+        e.target.parentNode.remove();
+    }
+});
+
+tasksList.addEventListener("dblclick", (e) => {
+    document.removeChild(e.targer);
 });
 
 
-//add event listeners to all tasks
+optionSelection.addEventListener("change", (e) => {
+    const tasks = document.querySelectorAll('.task');
+    console.log('onchange event occured')
+    hideAllTasks();
+    const currList = e.target.value;
+    if(currList === 'none') {
+        showAllTasks();
+    } else {
+        for (const task of tasks) {
+            console.log(`inside loop now curr task ${task}`);
+            if(task.classList.contains(currList)){
+                task.classList.remove('hidden');
+            }
+        }
+    }
+
+
+});
+
+
+function hideAllTasks() {
+    console.log("hideAllTasks happedned")
+    const tasks = document.querySelectorAll('.task');
+    for(const task of tasks){
+        console.log(`curr task: ${task}`);
+        task.classList.add('hidden');
+    }
+}
+
+
+
+function showAllTasks() {
+    console.log("showAllTasks happedned")
+    const tasks = document.querySelectorAll('.task');
+    for(const task of tasks){
+        task.classList.remove('hidden');
+    }
+}
